72896d9a4c2951737fa27765be0c63d1f0b6f3cb
Aaron Zhang <rabbit.aaron@gmail.com>
2017-01-02 02:56:18 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+#Method list
+
+* onClose, onError, onMessage,onOpen (to attach event listener)
+* getBinaryType, getBufferedAmount, getExtensions, getProtocol, getReadyState, getUrl (to get websocket properties)
+* hasOpened (returns true if websocket is opened, else return false)
+* close (close the websocket)
+* send(message) (send message to server)
+
+```
+//example
+(function() {
+    var sws = new SmartWebSocket('ws://127.0.0.1:8000');
+    sws.onMessage(function(e, sws) {
+        console.log('on message 1 called, data: ' + e.data);
+    }).onMessage(function(e, sws) {
+        console.log('on message 2 called, data: ' + e.data);
+    }).send('hello,world').send('hello,world 2');
+})();
+```
---
+++ SmartWebSocket.js
@@ -0,0 +1,65 @@
+let SmartWebSocket = (function() {
+    let makeEventFunc = (eventName) => function(callback) {
+        this.ws.addEventListener(eventName, (e) => this._invoke(callback, e), false);
+        return this;
+    };
+
+    let makeGetter = (attributeName) => function() {
+        this.ws[attributeName]
+    };
+
+    let capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+
+    class SmartWebSocket {
+
+        constructor(uri, protocols) {
+            this.ws = new WebSocket(uri, protocols);
+        }
+
+        _invoke(callback, event) {
+            callback.call(this, event, this);
+        }
+
+        isOpen() {
+            return this.getReadyState() === WebSocket.OPEN;
+        }
+
+        close() {
+            if (this.isOpen()) {
+                this.ws.close();
+            } else {
+                this.onOpen(function() {
+                    this.ws.close();
+                });
+            }
+            return this;
+        }
+
+        send(message) {
+            if (typeof message == 'object') {
+                message = JSON.stringify(message);
+            }
+            if (this.isOpen()) {
+                this.ws.send(message);
+            } else {
+                this.onOpen(function() {
+                    this.ws.send(message);
+                });
+            }
+            return this;
+        }
+    }
+
+
+    ['close', 'error', 'message', 'open'].forEach(
+        (eventName) =>
+        SmartWebSocket.prototype['on' + capitalize(eventName)] = makeEventFunc(eventName)
+    );
+
+    ['binaryType', 'bufferedAmount', 'extensions', 'protocol', 'readyState', 'url'].forEach(
+        (attributeName) =>
+        SmartWebSocket.prototype['get' + capitalize(attributeName)] = makeGetter(attributeName)
+    );
+
+    return SmartWebSocket;
+
